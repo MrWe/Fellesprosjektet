068a8cea9de7e09937e604829dd7ab6b0b10ec52
krislerv <kristoffer-lervik@hotmail.com>
2015-03-06 13:28:02 +0100

    cleanup

--- src/gui/NotificationController.java
+++ src/gui/NotificationController.java
@@ -1,8 +1,20 @@
 package gui;
 
+import database.DBConnection;
 import javafx.fxml.FXML;
 
-
 public class NotificationController {
+	
+	private MainApp mainApp;
+	private DBConnection db;
+
+	@FXML
+	private void initialize() {
+		db = new DBConnection();
+	}
+
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
 
 }

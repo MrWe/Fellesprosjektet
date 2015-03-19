f141b73f9875100f4fb2adeeee6b0929b5dfa14f
krislerv <kristoffer-lervik@hotmail.com>
2015-03-19 14:47:36 +0100

    reverted dynamic list size
    sorry trym :(

--- src/gui/ListController.java
+++ src/gui/ListController.java
@@ -21,10 +21,8 @@ public class ListController {
 	private MainApp mainApp;
 	private DBConnection db;
 	@FXML private TreeView<Group> treeView;
-	//@FXML private TreeItem treeItem;
 	@FXML private Button editGroupBtn;
 	@FXML private Button newSubGroupBtn;
-	
 	private TreeItem<Group> privateGroup;
 
 	@FXML
@@ -42,29 +40,22 @@ public class ListController {
 				ArrayList<String> groupMembers = new ArrayList<String>();
 				ArrayList<String> groupAdmins = new ArrayList<String>();
 				while (groupMembersRS.next()) {
-					if(groupMembersRS.getString("groupAdmin").equals("0")){
+					if (groupMembersRS.getString("groupAdmin").equals("0")){
 						groupMembers.add(groupMembersRS.getString("fullName"));
-					}else{
+					} else {
 						groupAdmins.add(groupMembersRS.getString("fullName"));
 					}
-
 				}
-				TreeItem<Group> treeItem = new TreeItem<Group>(new Group(AllGroupsRS.getString("groupName"), AllGroupsRS.getString("isPrivate").equals("1") ? true : false, AllGroupsRS.getString("usergroupID"), AllGroupsRS.getString("USERGROUP_usergroupID"), groupMembers, groupAdmins));
-				
-//				treeItem.expandedProperty().addListener(new ChangeListener<Boolean>() {
-//					@Override
-//					public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-//						System.out.println("efef");
-//						System.out.println("newValue = " + newValue);
-//						BooleanProperty bb = (BooleanProperty) observable;
-//						System.out.println("bb.getBean() = " + bb.getBean());
-//						TreeItem t = (TreeItem) bb.getBean();
-//						// Do whatever with t
-//					}
-//				});
-				
-				
-				//System.out.println(treeItem.getValue().isPrivateGroup());
+				TreeItem<Group> treeItem = new TreeItem<Group>(
+						new Group(
+								AllGroupsRS.getString("groupName"), 
+								AllGroupsRS.getString("isPrivate").equals("1") ? true : false, 
+								AllGroupsRS.getString("usergroupID"), 
+								AllGroupsRS.getString("USERGROUP_usergroupID"), 
+								groupMembers, 
+								groupAdmins
+						)
+				);
 				groups.put(AllGroupsRS.getString("usergroupID"), treeItem);
 			}
 			TreeItem<Group> root = groups.get("0"); // The root group is the one with usergroupID = 0
@@ -92,84 +83,40 @@ public class ListController {
 		treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<Group>>() {
 			@Override
 			public void changed(ObservableValue<? extends TreeItem<Group>> observableValue, TreeItem<Group> oldValue, TreeItem<Group> newValue) {
-				System.out.println("selection listener " + oldValue + " " + newValue);
-				if (!treeView.getRoot().getChildren().contains(oldValue)) {
-					System.out.println("oldValue not contained");
-				}
-				if (!treeView.getRoot().getChildren().contains(newValue)) {
-					System.out.println("newValue not contained");
-				}
-				// show the calendar of the chosen group
-				//System.out.println("chose another group");
 				if (newValue == null) {
 					return;
 				}
-				System.out.println("selectedItemProperty listener: " + newValue.getValue().getName());
-				if(newValue.getValue().isPrivateGroup() == true){
+				if (newValue.getValue().isPrivateGroup() == true) {
 					newSubGroupBtn.setDisable(true);
-				}else{
+				} else {
 					editGroupBtn.setDisable(false);
 					newSubGroupBtn.setDisable(false);
 				}
-				
-				if(!newValue.getValue().getAdmins().contains(mainApp.getUser().getName())){
+				if (!newValue.getValue().getAdmins().contains(mainApp.getUser().getName())){
 					newSubGroupBtn.setDisable(true);
 				};
-				
 				try {
 					if (!newValue.getValue().equals(treeView.getRoot().getValue())) {						
 						mainApp.showCalendar(newValue.getValue());
-					} else {
-						System.out.println("root group has been declined selection");
 					}
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
-//				treeView.setMinHeight(treeView.getExpandedItemCount()*37 + 2);
-//				treeView.setMaxHeight(treeView.getExpandedItemCount()*37 + 2);
 			}
 		});
+		// selects private group after a group has been deleted
 		treeView.getRoot().getChildren().addListener(new ListChangeListener<TreeItem<Group>>() {
-
 			@Override
 			public void onChanged(ListChangeListener.Change<? extends TreeItem<Group>> c) {
 				c.next();
-				if (c.getRemovedSize() > 0) { // a group was removed
+				if (c.getRemovedSize() > 0) { // if a group was removed
 					treeView.getSelectionModel().clearSelection();
 					treeView.getSelectionModel().select(privateGroup);
-				}
-				System.out.println("removed size: " + c.getRemovedSize());
-				
+				}				
 			}
 		});
-
-		//treeView.setMaxHeight(treeView.getExpandedItemCount()*37);		
-		//why the hell doesnt the codes below work?
-//		treeView.getSelectionModel().getSelectedItem().expandedProperty().addListener(new ChangeListener<Boolean>() {
-//			@Override
-//			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-//				System.out.println("efef");
-//				System.out.println("newValue = " + newValue);
-//				BooleanProperty bb = (BooleanProperty) observable;
-//				System.out.println("bb.getBean() = " + bb.getBean());
-//				TreeItem t = (TreeItem) bb.getBean();
-//				// Do whatever with t
-//			}
-//		});
-
-		treeView.expandedItemCountProperty().addListener(new ChangeListener() {
-
-			@Override
-			public void changed(ObservableValue observable, Object oldValue,
-					Object newValue) {
-				System.out.println(oldValue + " " + newValue);
-				treeView.setMinHeight(((int) oldValue)*37 + 2);
-				treeView.setMaxHeight(((int) oldValue)*37 + 2);
-			}
-		});
-		treeView.setMaxHeight(treeView.getExpandedItemCount()*37);
 	}
-	
+
 	public void selectPrivateGroup() {
 		treeView.getSelectionModel().clearSelection();
 		treeView.getSelectionModel().select(privateGroup);

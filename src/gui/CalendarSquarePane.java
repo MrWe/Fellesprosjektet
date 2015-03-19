d406bf75acf9fbb50e8a3e59f5a4bcedf02cbc26
krislerv <kristoffer-lervik@hotmail.com>
2015-03-19 13:11:36 +0100

    Scroll bar in csp when too many appointments

--- src/gui/CalendarSquarePane.java
+++ src/gui/CalendarSquarePane.java
@@ -1,14 +1,11 @@
 package gui;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import core.Appointment;
 import core.Group;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -21,8 +18,8 @@ public class CalendarSquarePane extends Pane {
 	public Text text;				// displays the date
 	private MainApp mainApp;
 	private VBox appointmentList;	// list of AppointmentSquarePane-objects
+	private ScrollPane scrollPane;
 	private Group group;			// the group that is currently selected in the list of groups
-	private ObservableList<Appointment> observableAppointments;  // list of appointments, currently does nothing
 	private String date;
 
 	public CalendarSquarePane(MainApp mainApp, String date, Group group) {
@@ -36,37 +33,29 @@ public class CalendarSquarePane extends Pane {
 		text.setFill(Color.WHITE);
 		this.getChildren().add(text);
 
-		List<Appointment> listAppointments = new ArrayList<Appointment>();					// ignore this stuff
-		observableAppointments = FXCollections.observableList(listAppointments);
-		observableAppointments.addListener(new ListChangeListener<Appointment>() {			// can be used to listen for changes in the observableList
-
-			@Override																		//
-			public void onChanged(ListChangeListener.Change<? extends Appointment> c) {
-				c.next();																//
-				//System.out.println(c.getAddedSize());
-				//System.out.println(c.getAddedSubList().get(0));							//
-			}
-		});																					//
-
-		// border between squares
 		this.setStyle("-fx-border-color: #124364;");
-		//this.setStyle("-fx-border-width: 0.3;");
 		this.setOnMousePressed(new EventHandler<MouseEvent>() { // when a square is pressed
 			@Override
 			public void handle(MouseEvent event) {
-				//.out.println(event.getSource() + " " + event.getTarget());
 				if (event.getTarget() instanceof AppointmentSquarePane) {
 					showPopup((AppointmentSquarePane) event.getTarget());
 				} else {					
 					showPopup();
+					System.out.println(appointmentList.getHeight() + " " +scrollPane.getHeight());
 				}
 			}
-		});		
+		});	
 		appointmentList = new VBox();
-		appointmentList.setLayoutX(1);
-		appointmentList.setLayoutY(20);
+		scrollPane = new ScrollPane(appointmentList);
+		scrollPane.setLayoutY(20);
+		scrollPane.setPrefSize(74, 60);
+		scrollPane.setFocusTraversable(false);
+		appointmentList.setAlignment(Pos.TOP_LEFT);
+		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		scrollPane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
 		appointmentList.setSpacing(2);
-		this.getChildren().add(appointmentList);
+		
+		this.getChildren().add(scrollPane);
 
 		Rectangle clipRectangle = new Rectangle(); // makes sure the contents of a square is not shown when the square is too small to display it
 		this.setClip(clipRectangle);
@@ -74,7 +63,8 @@ public class CalendarSquarePane extends Pane {
 			text.setLayoutX(newValue.getWidth() - text.getLayoutBounds().getWidth() -2);
 			clipRectangle.setWidth(newValue.getWidth());
 			clipRectangle.setHeight(newValue.getHeight());
-			appointmentList.setPrefSize(newValue.getWidth() - 2, newValue.getHeight() - 2);
+			scrollPane.setPrefSize(newValue.getWidth(), newValue.getHeight() - 20);
+			appointmentList.setPrefWidth(newValue.getWidth());
 		});
 	}
 
@@ -86,7 +76,6 @@ public class CalendarSquarePane extends Pane {
 		mainApp.showAppointmentPopup(this, null, group);
 	}
 
-	//edit en appointment
 	private void showPopup(AppointmentSquarePane asp) {
 		mainApp.showEditAppointmentPopup(null, asp, group);
 	}
@@ -94,14 +83,13 @@ public class CalendarSquarePane extends Pane {
 	public void addAppointment(Appointment appointment) {
 		AppointmentSquarePane asp = new AppointmentSquarePane(appointment, date);
 		appointmentList.getChildren().add(asp);
-		observableAppointments.add(asp.getAppointment());
 	}
-	
+
 	@Override
 	public String toString() {
 		return super.toString() + " " + date;
 	}
-	
+
 	public Text getText(){
 		return text;
 	}
